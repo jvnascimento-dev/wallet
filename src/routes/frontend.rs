905827b899b19883705c318cacbff7551ef3c961
Vitor Nascimento <joaovitornas1@gmail.com>
2026-07-28 17:01:06 -0400

    FEAT: Adicionado rota fn assets.

--- src/routes/frontend.rs
+++ src/routes/frontend.rs
@@ -2,6 +2,7 @@ use crate::{
     app::AppState,
     auth::user::{UnauthUser, User},
     error::AppErr,
+    models::{Asset, OwnedAsset},
     repository::Repository,
 };
 use askama::Template;
@@ -12,11 +13,13 @@ use axum::{
 };
 use axum_extra::extract::{CookieJar, cookie::Cookie};
 use serde::Deserialize;
+use tokio::try_join;
 
 pub fn router() -> Router<AppState> {
     Router::new()
         .route("/", get(index))
         .route("/login", get(login_page).post(login))
+        .route("/logout", get(logout))
 }
 
 #[derive(Template)]
@@ -59,4 +62,27 @@ async fn index(maybe_user: Option<User>) -> Result<Response, AppErr> {
 }
 async fn logout(jar: CookieJar) -> impl IntoResponse {
     (jar.remove("token"), Redirect::to("/login"))
+}
+
+#[derive(Template)]
+#[template(path = "assets.html")]
+pub struct AssetsPage {
+    owned_assets: Vec<OwnedAsset>,
+    available_assets: Vec<Asset>,
+    user: User,
+}
+
+async fn assets(repository: Repository, user: User) -> Result<Html<String>, AppErr> {
+    let (owned_assets, available_assets) = try_join!(
+        repository.list_owned_assets(user.id()),
+        repository.list_assets()
+    )?;
+
+    let html = AssetsPage {
+        owned_assets,
+        available_assets,
+        user,
+    }
+    .render()?;
+    Ok(Html(html))
 }
